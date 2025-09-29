62362a2f6340437f2641236f7e740c47139370c2
agsdearmas <ags.dearmas@gmail.com>
2025-09-29 19:07:23 -0300

    Refactor: remove hardcoded DB credentials in docker-compose

--- docker/run-all.sh
+++ docker/run-all.sh
@@ -3,9 +3,9 @@
 echo "> Iniciando servicios con Docker Compose..."
 
 # Reconstrir la imagen de la app si hay cambios en el codigo/Dockerfile
-docker-compose up -d --build
+docker-compose -f docker-compose.yml --env-file ../.env up -d --build
 
 echo "> Servicios iniciados. Verificando logs (Ctrl+C para salir de los logs, los contenedores seguiran activos)..."
 
 # Mostrar logs combinados de ambos servicios, sin detiene la ejecucion del script
-docker-compose logs -f
+docker-compose -f docker-compose.yml --env-file ../.env logs -f
